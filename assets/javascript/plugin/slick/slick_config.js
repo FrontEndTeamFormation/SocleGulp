42377028e764bba2ccf5d2355be21495cae694c5
amine <amami.amami@iit.ens.tn>
2018-02-14 22:46:21 +0100

    header,section,footer done commit

---
+++ assets/javascript/plugin/slick/slick_config.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('.my-slider').slick({
+        autoplay: true,
+        accessibility: true,
+        autoplaySpeed: 2000,
+        //    arrows: true,
+        //dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
+
+// On swipe event
+$('.my-slider').on('swipe', function (event, slick, direction) {
+    console.log(direction);
+    // left
+});
+
+// On edge hit
+$('.my-slider').on('edge', function (event, slick, direction) {
+    console.log('edge was hit')
+});
